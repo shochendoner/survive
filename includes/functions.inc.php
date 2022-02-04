3508d9386bf8ab8803ec6e4a532464fa20f1a933
shochendoner <shochendoner@gmail.com>
2022-02-04 11:52:36 -0500

    commit

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -75,43 +75,6 @@ function uidExists($conn, $username) {
 }
 
 // Display Picks
-function getUserPicks ($id)
-{
-	$array = array();
-	$q = mysqli_query("SELECT * FROM 'users' WHERE 'usersID'=".$id);
-	while ($r = mysqli_fetch_assoc($q))
-	{
-			$array['usersId'] = $row['usersId'];
-
-
-
-	}
-
-	)
-
-}
-
-
-
-// Insert picks into database --STILL FIXING 
-function makePicks($pick1, $pick2) {
-	$sql = "INSERT INTO users (pickOne, pickTwo) VALUES (?, ?);";
-	
-	$stmt = mysqli_stmt_init($conn);
-	if (!mysqli_stmt_prepare($stmt, $sql)) {
-	 	header("location: ../signup.php?error=stmtfailed");
-		exit();
-	}
-
-	$hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
-
-	mysqli_stmt_bind_param($stmt, "ssss", $name, $email, $username, $hashedPwd);
-	mysqli_stmt_execute($stmt);
-	mysqli_stmt_close($stmt);
-	mysqli_close($conn);
-	header("location: ../signup.php?error=none");
-	exit();
-}
 
 // Insert new user into database
 function createUser($conn, $name, $email, $username, $pwd) {
---
+++ wip.php
@@ -0,0 +1,30 @@
+// Make Pics Form 
+
+<div>
+      <form action="includes/makepics.inc.php" method="post">
+        <input type="checkbox" name="checkbox_name" value="#1 Baylor">
+        <button type="submit" name="submit">Submit</button>
+      </form>
+  </div>
+
+  // Insert picks into database --STILL FIXING 
+function makePicks($pick1, $pick2) {
+	$sql = "INSERT INTO users (pickOne, pickTwo) VALUES (?, ?);";
+	
+	$stmt = mysqli_stmt_init($conn);
+	if (!mysqli_stmt_prepare($stmt, $sql)) {
+	 	header("location: ../signup.php?error=stmtfailed");
+		exit();
+	}
+
+	$hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
+
+	mysqli_stmt_bind_param($stmt, "ssss", $name, $email, $username, $hashedPwd);
+	mysqli_stmt_execute($stmt);
+	mysqli_stmt_close($stmt);
+	mysqli_close($conn);
+	header("location: ../signup.php?error=none");
+	exit();
+}
+
+
