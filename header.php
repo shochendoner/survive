795eb865d3f8e90aec7b3c10a968615247e8286e
shochendoner <shochendoner@gmail.com>
2022-02-08 07:31:44 -0800

    Add files via upload

--- header.php
+++ header.php
@@ -1,7 +1,6 @@
 <?php
   session_start();
   include_once 'includes/functions.inc.php';
-
 ?>
 
 <!DOCTYPE html>
@@ -23,14 +22,14 @@
         <ul>
           <li><a href="index.php">Home</a></li>
           <?php
-          if (isset($_SESSION["useruid"])) {
-            echo "<li><a href='profile.php'>Profile Page</a></li>";
-            echo "<li><a href='logout.php'>Logout</a></li>";
-          }
-          else {
-            echo "<li><a href='signup.php'>Sign up</a></li>";
-            echo "<li><a href='login.php'>Log in</a></li>";
-          }
+            if (isset($_SESSION["useruid"])) {
+              echo "<li><a href='profile.php'>Profile Page</a></li>";
+              echo "<li><a href='logout.php'>Logout</a></li>";
+            }
+            else {
+              echo "<li><a href='signup.php'>Sign up</a></li>";
+              echo "<li><a href='login.php'>Log in</a></li>";
+            }
           ?>
         </ul>
       </div>
--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -74,41 +74,6 @@ function uidExists($conn, $username) {
 	mysqli_stmt_close($stmt);
 }
 
-// Display Picks
-
-
-// Make Pics
-
-if (isset($_POST["submit"])) {
-
-  // First we get the form data from the URL
-  $pickOne = $_POST["pickOne"];
-  $pickTwo = $_POST["pickTwo"];
-  $username = $_POST["uid"];
- 
-
-  // Then we run a bunch of error handlers to catch any user mistakes we can (you can add more than I did)
-  // These functions can be found in functions.inc.php
-
-  require_once "dbh.inc.php";
-  require_once 'functions.inc.php';
-
-  // Left two picks empty
-  // We set the functions "!== false" since "=== true" has a risk of giving us the wrong outcome
-  if (emptyInputSignup($pickOne, $pickTwo) !== false) {
-    header("location: ../signup.php?error=emptyinput");
-		exit();
-  }
-  // If we get to here, it means there are no user errors
-
-  // Now we insert the user into the database
-  makePicks($pickOne, $pickTwo);
-
-} else {
-	header("location: ../signup.php");
-    exit();
-}
-
 // Insert new user into database
 function createUser($conn, $name, $email, $username, $pwd) {
   $sql = "INSERT INTO users (usersName, usersEmail, usersUid, usersPwd) VALUES (?, ?, ?, ?);";
